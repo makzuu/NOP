400c5a203816f1914fba35b4b2547b861834dbef
makzuu <mr.greenmousth@gmail.com>
2026-05-14 22:38:25 -0400

    going places

---
+++ program.t
@@ -0,0 +1,6 @@
+# read a value from the top of the stack into ACC
+MOV IN, ACC
+# add the value in ACC to itself, doubling it
+ADD ACC
+# write the value in the ACC register to the console
+MOV ACC, OUT
---
+++ project.py
@@ -0,0 +1,55 @@
+import sys
+
+
+class Ins:
+    def __init__(self, name, ln, params):
+        self.name = name
+        self.ln = ln
+        self.params = params
+    
+    def __str__(self):
+        return f"{self.ln} {self.name}: {self.params}"
+
+def main():
+    program = preprocess()
+    for ins in program:
+        print(ins)
+
+def preprocess():
+    program = []
+    with open("program.t") as f:
+        ln = -1
+        for ins in f:
+            ln += 1
+            ins = ins.strip()
+            if ins.startswith("#") or ins == "":
+                continue
+            name, *params = ins.split(" ")
+            for i in range(len(params)):
+                params[i] = params[i].rstrip(",")
+
+            if name == "MOV":
+                if len(params) != 2:
+                    sys.exit(f"L: {ln} MOV: 2 parameter(s) expected, {len(params)} found")
+
+                program.append(Ins("MOV", ln, params))
+            elif name == "ADD":
+                if len(params) != 1:
+                    sys.exit(f"L: {ln} MOV: 2 parameter(s) expected, {len(params)} found")
+                program.append(Ins("ADD", ln, params))
+
+            else:
+                sys.exit(f"L: {ln} {name}: invalid instruction")
+
+    return program
+
+def mov():
+    ...
+
+
+def add():
+    ...
+
+
+if __name__ == "__main__":
+    main()
