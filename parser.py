9866e16f3e12c643b8779d29fb2ea6278a1c61e6
makzuu <mr.greenmousth@gmail.com>
2026-06-06 16:03:28 -0400

    Squashed commit of the following:
    commit 1b553c8c9e75c18b3375defe1bc53bfed83e712e
    Author: makzuu <mr.greenmousth@gmail.com>
    Date:   Thu Jun 4 22:35:31 2026 -0400
    
        Add Evaluator
    
    commit 3d3416dd10bf0e7314a41d37b20d6bba9deaf01e
    Author: makzuu <mr.greenmousth@gmail.com>
    Date:   Wed Jun 3 15:33:51 2026 -0400
    
        commit parcial

--- parser.py
+++ parser.py
@@ -2,8 +2,9 @@ from lexer import *
 import sys
 
 class Parser:
-    def __init__(self, lexer):
+    def __init__(self, lexer, eval):
         self.lexer = lexer
+        self.eval = eval
 
         self.cur_token = None
         self.next_token()
@@ -45,62 +46,95 @@ class Parser:
         print("STATEMENT")
         # ident ":"
         if self.check_type(TokenType.IDENT):
+            self.eval.add_label(self.cur_token.text)
             self.next_token()
             self.match_type(TokenType.COLON)
         # "MOV" src "," dst
         elif self.check_type(TokenType.MOV):
+            self.eval.add_ins("MOV")
             self.next_token()
             self.src()
             self.match_type(TokenType.COMMA)
             self.dst()
+            self.eval.done()
         # "SWP"
         elif self.check_type(TokenType.SWP):
+            self.eval.add_ins("SWP")
+            self.eval.done()
             self.next_token()
         # "SAV"
         elif self.check_type(TokenType.SAV):
+            self.eval.add_ins("SAV")
+            self.eval.done()
             self.next_token()
         # "ADD" src
         elif self.check_type(TokenType.ADD):
+            self.eval.add_ins("ADD")
             self.next_token()
             self.src()
+            self.eval.done()
         # "SUB" src
         elif self.check_type(TokenType.SUB):
+            self.eval.add_ins("SUB")
             self.next_token()
             self.src()
+            self.eval.done()
         # "NEG"
         elif self.check_type(TokenType.NEG):
+            self.eval.add_ins("NEG")
+            self.eval.done()
             self.next_token()
         # "JMP" ident
         elif self.check_type(TokenType.JMP):
+            self.eval.add_ins("JMP")
             self.next_token()
+            self.eval.add_param(self.cur_token.text)
             self.match_type(TokenType.IDENT)
+            self.eval.done()
         # "JEZ" ident
         elif self.check_type(TokenType.JEZ):
+            self.eval.add_ins("JEZ")
             self.next_token()
+            self.eval.add_param(self.cur_token.text)
             self.match_type(TokenType.IDENT)
+            self.eval.done()
         # "JNZ" ident
         elif self.check_type(TokenType.JNZ):
+            self.eval.add_ins("JNZ")
             self.next_token()
+            self.eval.add_param(self.cur_token.text)
             self.match_type(TokenType.IDENT)
+            self.eval.done()
         # "JGZ" ident
         elif self.check_type(TokenType.JGZ):
+            self.eval.add_ins("JGZ")
             self.next_token()
+            self.eval.add_param(self.cur_token.text)
             self.match_type(TokenType.IDENT)
+            self.eval.done()
         # "JLZ" ident
         elif self.check_type(TokenType.JLZ):
+            self.eval.add_ins("JLZ")
             self.next_token()
+            self.eval.add_param(self.cur_token.text)
             self.match_type(TokenType.IDENT)
+            self.eval.done()
         # "JRO" src
         elif self.check_type(TokenType.JRO):
+            self.eval.add_ins("JRO")
             self.next_token()
             self.src()
+            self.eval.done()
         # "PRINT" readable | string
         elif self.check_type(TokenType.PRINT):
+            self.eval.add_ins("PRINT")
             self.next_token()
             if self.check_type(TokenType.STRING):
+                self.eval.add_param(self.cur_token.text)
                 self.next_token()
             else:
                 self.readable()
+            self.eval.done()
         else:
             self.print_and_exit(f"invalid token ({self.cur_token.text})")
 
@@ -110,6 +144,7 @@ class Parser:
     def src(self):
         print("SRC")
         if self.check_type(TokenType.NUMBER):
+            self.eval.add_param(self.cur_token.text)
             self.next_token()
         else:
             self.readable()
@@ -121,10 +156,13 @@ class Parser:
     def readable(self):
         print("READABLE")
         if self.check_type(TokenType.ACC):
+            self.eval.add_param(self.cur_token.text)
             self.next_token()
         elif self.check_type(TokenType.IN):
+            self.eval.add_param(self.cur_token.text)
             self.next_token()
         elif self.check_type(TokenType.STACK):
+            self.eval.add_param(self.cur_token.text)
             self.next_token()
         else:
             self.print_and_exit(f"invalid token ({self.cur_token.text})")
@@ -132,12 +170,16 @@ class Parser:
     def writable(self):
         print("WRITABLE")
         if self.check_type(TokenType.ACC):
+            self.eval.add_param(self.cur_token.text)
             self.next_token()
         elif self.check_type(TokenType.OUT):
+            self.eval.add_param(self.cur_token.text)
             self.next_token()
         elif self.check_type(TokenType.STACK):
+            self.eval.add_param(self.cur_token.text)
             self.next_token()
         elif self.check_type(TokenType.SCREEN):
+            self.eval.add_param(self.cur_token.text)
             self.next_token()
         else:
             self.print_and_exit(f"invalid token ({self.cur_token.text})")
