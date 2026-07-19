6384ecf0a022aab2c4c8e89fc4b8c4f740905040
makzuu <mr.greenmousth@gmail.com>
2026-07-19 01:32:41 -0400

    Experiment with a parser tree #9

--- parser.py
+++ parser.py
@@ -1,11 +1,19 @@
 import logger as log
 from token import TokenType
 
+class Node:
+    def __init__(self, token=None):
+        self.children = []
+        self.token = token
+
 class Parser:
     def __init__(self, lexer, state, eval):
         self.lexer = lexer
         self.state = state
         self.eval = eval
+
+        self.tree = Node()
+        self.cur_node = self.tree
 
         self.cur_token = None
         self.next_token()
@@ -26,80 +34,146 @@ class Parser:
         while self.cur_token.type == TokenType.NL:
             self.next_token()
 
+    def add_node(self, node):
+        self.cur_node.children.append(node)
+    
+    @staticmethod
+    def print_tree(node, depth=0):
+        if node.token != None:
+            print("\t" * depth, str(node.token.text))
+        for child in node.children:
+            Parser.print_tree(child, depth+1)
+
     def program(self):
         self.skip_nl()
 
         while self.cur_token.type != TokenType.EOF:
             self.statement()
 
+        Parser.print_tree(self.tree)
+
     def statement(self):
-        print("STATEMENT")
+        self.cur_node = self.tree
         if self.check_type(TokenType.NOP):
+            self.add_node(Node(self.cur_token))
             self.next_token()
         elif self.check_type(TokenType.MOV):
+            node = Node(self.cur_token)
+            self.add_node(node)
+            self.cur_node = node
             self.next_token()
             self.src()
             self.match_type(TokenType.COMMA)
+            self.cur_node = node
             self.dst()
         elif self.check_type(TokenType.SWP):
+            self.add_node(Node(self.cur_token))
             self.next_token()
         elif self.check_type(TokenType.SAV):
+            self.add_node(Node(self.cur_token))
             self.next_token()
         elif self.check_type(TokenType.ADD):
+            node = Node(self.cur_token)
+            self.add_node(node)
+            self.cur_node = node
             self.next_token()
             self.src()
         elif self.check_type(TokenType.SUB):
+            node = Node(self.cur_token)
+            self.add_node(node)
+            self.cur_node = node
             self.next_token()
             self.src()
         elif self.check_type(TokenType.NEG):
+            self.add_node(Node(self.cur_token))
             self.next_token()
         elif self.check_type(TokenType.JMP):
+            node = Node(self.cur_token)
+            self.add_node(node)
             self.next_token()
+            node.children.append(Node(self.cur_token))
             self.match_type(TokenType.IDENT)
         elif self.check_type(TokenType.JEZ):
+            node = Node(self.cur_token)
+            self.add_node(node)
             self.next_token()
+            node.children.append(Node(self.cur_token))
             self.match_type(TokenType.IDENT)
         elif self.check_type(TokenType.JNZ):
+            node = Node(self.cur_token)
+            self.add_node(node)
             self.next_token()
+            node.children.append(self.cur_token)
             self.match_type(TokenType.IDENT)
         elif self.check_type(TokenType.JGZ):
+            node = Node(self.cur_token)
+            self.add_node(node)
             self.next_token()
+            node.children.append(self.cur_token)
             self.match_type(TokenType.IDENT)
         elif self.check_type(TokenType.JLZ):
+            node = Node(self.cur_token)
+            self.add_node(node)
             self.next_token()
+            node.children.append(self.cur_token)
             self.match_type(TokenType.IDENT)
         elif self.check_type(TokenType.JRO):
+            node = Node(self.cur_token)
+            self.add_node(node)
+            self.cur_node = node
             self.next_token()
             self.src()
         # | ident ":"
         elif self.check_type(TokenType.IDENT):
+            self.add_node(Node(self.cur_token))
             self.next_token()
             self.match_type(TokenType.COLON)
             if self.check_type(TokenType.NL):
                 self.nl()
             return
         elif self.check_type(TokenType.PUSH):
+            node = Node(self.cur_token)
+            self.add_node(node)
+            self.cur_node = node
             self.next_token()
             self.src()
         elif self.check_type(TokenType.POP):
+            node = Node(self.cur_token)
+            self.add_node(node)
+            self.cur_node = node
             self.next_token()
             self.dst()
         elif self.check_type(TokenType.READ):
+            self.add_node(Node(self.cur_token))
             self.next_token()
         elif self.check_type(TokenType.WRITE):
+            node = Node(self.cur_token)
+            self.add_node(node)
+            self.cur_node = node
             self.next_token()
             self.src()
         elif self.check_type(TokenType.DEFINE):
+            node = Node(self.cur_token)
+            self.add_node(node)
+            self.cur_node = node
             self.next_token()
+            self.add_node(Node(self.cur_token))
             self.match_type(TokenType.IDENT)
             self.match_type(TokenType.COMMA)
+            self.add_node(Node(self.cur_token))
             self.match_type(TokenType.NUMBER)
         elif self.check_type(TokenType.CALL):
+            node = Node(self.cur_token)
+            self.add_node(node)
+            self.cur_node = node
             self.next_token()
+            self.add_node(Node(self.cur_token))
             self.match_type(TokenType.IDENT)
         elif self.check_type(TokenType.RET):
+            self.add_node(Node(self.cur_token))
             self.next_token()
         elif self.check_type(TokenType.DRAW):
+            self.add_node(Node(self.cur_token))
             self.next_token()
         else:
             log.error(f"invalid token ({self.cur_token.text})", self.cur_token.line)
@@ -107,22 +181,30 @@ class Parser:
         self.nl()
 
     def src(self):
-        print("SRC")
+        node = Node()
+        self.add_node(node)
+        self.cur_node = node
         if self.check_type(TokenType.ACC):
+            self.add_node(Node(self.cur_token))
             self.next_token()
         elif self.check_type(TokenType.NIL):
+            self.add_node(Node(self.cur_token))
             self.next_token()
         elif self.check_type(TokenType.NUMBER):
+            self.add_node(Node(self.cur_token))
             self.next_token()
         elif self.check_type(TokenType.IDENT):
+            self.add_node(Node(self.cur_token))
             self.next_token()
         elif self.check_type(TokenType.BP):
+            self.add_node(Node(self.cur_token))
             self.next_token()
             if self.check_type(TokenType.OPEN_BRACKET):
                 self.next_token()
                 self.src()
                 self.match_type(TokenType.CLOSE_BRACKET)
         elif self.check_type(TokenType.SP):
+            self.add_node(Node(self.cur_token))
             self.next_token()
             if self.check_type(TokenType.OPEN_BRACKET):
                 self.next_token()
@@ -132,20 +214,27 @@ class Parser:
             log.error(f"invalid token ({self.cur_token.text})", self.cur_token.line)
 
     def dst(self):
-        print("DST")
+        node = Node()
+        self.add_node(node)
+        self.cur_node = node
         if self.check_type(TokenType.ACC):
+            self.add_node(Node(self.cur_token))
             self.next_token()
         elif self.check_type(TokenType.NIL):
+            self.add_node(Node(self.cur_token))
             self.next_token()
         elif self.check_type(TokenType.IDENT):
+            self.add_node(Node(self.cur_token))
             self.next_token()
         elif self.check_type(TokenType.BP):
+            self.add_node(Node(self.cur_token))
             self.next_token()
             if self.check_type(TokenType.OPEN_BRACKET):
                 self.next_token()
                 self.src()
                 self.match_type(TokenType.CLOSE_BRACKET)
         elif self.check_type(TokenType.SP):
+            self.add_node(Node(self.cur_token))
             self.next_token()
             if self.check_type(TokenType.OPEN_BRACKET):
                 self.next_token()
--- spec.md
+++ spec.md
@@ -1,31 +1,29 @@
 ```
 program ::= {statement}
 
-statement ::=
-	| nop nl
-	| mov src "," dst nl
-	| swp nl
-	| sav nl
-	| add src nl
-	| sub src nl
-	| neg nl
-	| jmp ident nl
-	| jez ident nl
-	| jnz ident nl
-	| jgz ident nl
-	| jlz ident nl
-	| jro src nl
+statement ::= nop nl
+    | mov src "," dst nl
+    | swp nl
+    | sav nl
+    | add src nl
+    | sub src nl
+    | neg nl
+    | jmp ident nl
+    | jez ident nl
+    | jnz ident nl
+    | jgz ident nl
+    | jlz ident nl
+    | jro src nl
 
-	| ident ":"
-	| push src nl
-	| pop dst nl
-	| read nl
-	| write src nl
-	| define ident "," number nl
-	| call ident nl
-	| ret nl
+    | ident ":"
+    | push src nl
+    | pop dst nl
+    | read nl
+    | write src nl
+    | define ident "," number nl
+    | call ident nl
+    | ret nl
     | draw nl
-
 
 src ::= acc
     | nil
